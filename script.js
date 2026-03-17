94f5bfeb3f1f92c5619eac63e4be9ae5be8fa15d
Linda Albrektsson <linda.albrektsson@gmail.com>
2026-03-17 13:10:58 +0100

    renderMovies med bild

--- script.js
+++ script.js
@@ -17,14 +17,29 @@ const API_MOVIES = "http://localhost:3000/movies";
 const API_CATEGORIES = "http://localhost:3000/categories";
 
 async function getMovies() {
-  const response = await fetch("http://localhost:3000/movies");
-  const movies = await response.json();
+  try {
+    const response = await fetch("http://localhost:3000/movies");
 
+    if (!response.ok) {
+      throw new Error("Kunde inte hämta filmer.");
+    }
+
+    const movies = await response.json();
+    renderMovies(movies);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getMovies();
+
+function renderMovies(movies) {
   moviesContainer.innerHTML = "";
 
   movies.forEach((movie) => {
     moviesContainer.innerHTML += `
       <div>
+        <img src="${movie.image}" alt="${movie.title}">
         <h3>${movie.title}</h3>
         <p>År: ${movie.year}</p>
         <p>Betyg: ${movie.rating}</p>
@@ -32,5 +47,3 @@ async function getMovies() {
     `;
   });
 }
-
-getMovies();
